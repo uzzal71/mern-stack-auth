bf67340afd8c97d6925496ac5fc3bfd636b756a8
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-15 02:02:56 +0600

    pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Auth app</h1>
+      <Routes>
+        <Route path='/' element={<Navigate to='/login' />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
