f8cc718bd0e6c323406adb42566acf3b03896bb6
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-15 00:15:22 +0600

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors")
 const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
 
 const app = express();
 require("dotenv").config();
@@ -16,6 +17,7 @@ app.get("/ping", (req, res) => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
