7aee241f5ae1064722c76de2c463e5527121114e
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-14 23:58:16 +0600

    auth complete in backend

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
+const {signupValidation, loginValidation} = require("../Middlewares/AuthValidation");
+const {signup, login} = require("../Controllers/AuthController");
 
-router.post("/login", (req, res) => {
-    res.send("login success");
-});
-
-router.post("signup", (req, res) => {
-    res.send("signup success");
-});
+router.post("/login", loginValidation, login);
+router.post("/signup", signupValidation, signup);
 
 module.exports = router;
