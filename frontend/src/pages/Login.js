26d1785d9d89f564476dbc1c7250abd1f5af1926
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-15 16:02:30 +0600

    Signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const copyLoginInfo = { ...loginInfo };
+    copyLoginInfo[name] = value;
+    setLoginInfo(copyLoginInfo);
+  };
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            autoFocus
+            placeholder="Enter your email"
+            onChange={handleChange}
+            value={loginInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            autoFocus
+            placeholder="********"
+            onChange={handleChange}
+            value={loginInfo.password}
+          />
+        </div>
+        <button>Login</button>
+        <span>
+          Does't have an account ?<Link to="/signup">Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError, handleSuccess } from "../utils";
 
 function Signup() {
+  const [signupInfo, setSignupInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const copySignupInfo = { ...signupInfo };
+    copySignupInfo[name] = value;
+    setSignupInfo(copySignupInfo);
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = signupInfo;
+    if (!name || !email || !password) {
+      return handleError("name, email and password are required");
+    }
+
+    try {
+      const url = "http://localhost:3002/auth/signup";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInfo),
+      });
+      const result = await response.json();
+      const { message, user, token, errors } = result;
+      if (token) {
+        handleSuccess(message);
+        localStorage.setItem("token", token);
+        localStorage.setItem("loggedInUser", user);
+        setTimeout(() => {
+          navigate("/home");
+        }, 1000);
+      } else if(errors) {
+        handleError(errors[0]);
+      } else {
+        handleError("Internal server error");
+      }
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <div className="container">
+      <h1>Signup</h1>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            autoFocus
+            placeholder="Enter your name"
+            onChange={handleChange}
+            value={signupInfo.name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            autoFocus
+            placeholder="Enter your email"
+            onChange={handleChange}
+            value={signupInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            autoFocus
+            placeholder="********"
+            onChange={handleChange}
+            value={signupInfo.password}
+          />
+        </div>
+        <button type="submit">Signup</button>
+        <span>
+          Already have an account?
+          <Link to="/login">Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
