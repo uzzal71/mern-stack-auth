be594c5490d15c4e1c64104cc58884e6b2c7b8ff
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-15 16:37:47 +0600

    private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import RefrshHandler from './RefrshHandler';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
   return (
     <div className="App">
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path='/' element={<Navigate to='/login' />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<Signup />}/>
-        <Route path='/home' element={<Home />}/>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError, handleSuccess } from "../utils";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-export default Home
+  useEffect(() => {
+    setLoggedInUser(JSON.parse(localStorage.getItem("loggedInUser")));
+  }, []);
+
+  const handleLogout = (e) => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("loggedInUser");
+    handleSuccess("User Loggedout");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+  };
+
+  const fetchProducts = async () => {
+    try {
+      const url = "http://localhost:3002/products";
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      };
+      const response = await fetch(url, headers);
+      const result = await response.json();
+      console.log(result);
+      setProducts(result);
+    } catch (err) {
+      handleError(err);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome {loggedInUser?.name}</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <div>
+        {products &&
+          products.length > 0 &&
+          products?.map((item, index) => (
+            <ul key={index}>
+              <span>
+                {item.name} : {item.price}
+              </span>
+            </ul>
+          ))}
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Home;
